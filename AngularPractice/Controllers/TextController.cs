da8ecf2ecdc1082d208f804757a07e6570be6aed
cmcintosh <cmcintosh@loopbackanalytics.com>
2016-12-01 13:19:35 -0600

    Angular Practice Project / Service Injector Practice
    I am playing with angular with the goal of having a better understanding
    of $http service injector requests

---
+++ AngularPractice/Controllers/TextController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Web.Http;
+
+namespace AngularPractice.Controllers
+{
+	public class TextController : ApiController
+	{
+		[HttpGet]
+		[Route("api/Text/Success/{message}")]
+		public IHttpActionResult Success(string message)
+		{
+			return Json(message);
+		}
+
+		[HttpGet]
+		[Route("api/Text/Fail/{message}")]
+		public IHttpActionResult Fail(string message)
+		{
+			throw new ApplicationException("Bad news bears!");
+		}
+	}
+}
---
+++ AngularPractice/HelloWorldApp/hello_world_controller.js
@@ -0,0 +1,61 @@
+﻿var app = angular.module('HelloWorldApp', []);
+
+app.controller('HelloWorldController', function($scope, $window, $http, $log) {
+    $scope.greeting = "Hello!";
+
+    $scope.go = function (shouldFail) {
+        if (shouldFail) {
+            //$http.get('/api/Text/Fail').then(function(response) {
+            //    $scope.responseMessage = response;
+            //    $scope.errorMessage = '';
+            //});
+
+            $http.get('/api/Text/Fail')
+                .success(function(response) {
+                    $scope.responseMessage = response;
+                    $scope.errorMessage = '';
+                }).error(function (response) {
+                    $scope.errorMessage = 'An error has occurred.\n Message: \n' + response.Message + 'Message Detail: ' + response.MessageDetail;
+                    $scope.responseMessage = '';
+            });
+        } else {
+            //$http.get('/api/Text/Fail').then(function(response) {
+            //    $scope.responseMessage = response;
+            //    $scope.errorMessage = '';
+            //});
+
+            $http.get('/api/Text/Success/HelloFromMvc')
+                .success(function (response) {
+                    $scope.responseMessage = response;
+                    $scope.errorMessage = '';
+                }).error(function (response) {
+                    $scope.errorMessage = 'An error has occurred.\n Message: \n' + response.Message + 'Message Detail: ' + response.MessageDetail;
+                    $scope.responseMessage = '';
+                });
+        }
+    }
+
+    $scope.$log = $log;
+    $scope.message = "hello world!";
+});
+
+app.factory('practiceFactory', function () {
+    return {
+        sayHello: function (text) {
+            return "Factory says \"Hello " + text + "\"";
+        },
+        sayGoodbye: function (text) {
+            return "Factory says \"Goodbye " + text + "\"";
+        }
+    }
+});
+
+function HelloCtrl($scope, practiceFactory) {
+    $scope.fromFactory = practiceFactory.sayHello("World");
+};
+function GoodbyeCtrl($scope, practiceFactory) {
+    $scope.fromFactory = practiceFactory.sayGoodbye("World");
+};
+
+
+
